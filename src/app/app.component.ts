6fcd5383b18a81fafd02d54025454538d49f8dce
Khaled Jami <mkjami.1998@gmail.com>
2020-07-21 16:00:56 +0530

    accessing form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,48 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  answer:string;
   @ViewChild('f',null) signUpForm:NgForm;
+
+  answer:string;
+  genders=['male','female'];
+
+  user={
+    username:'',
+    email:'',
+    secretQuestion:'',
+    answer:'',
+    gender:''
+  };
+  submitted=false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signUpForm.setValue({         //to fill all form, we use setValue.
+    //   userData:{
+    //     username:suggestedName,
+    //     email:'user@gmail.com'
+    //   },
+    //   secret:'pet',
+    //   QuestionAnswer:'Your answer here!',
+    //   gender:'male'
+    // });
+    this.signUpForm.form.patchValue({     //to fill part of form, we use patchValue.
+      userData:{
+        username:suggestedName
+      }
+    })
   }
   // onSubmit(form: NgForm){
   //   console.log(form);
   // }
   onSubmit(form: NgForm){
-    console.log(this.signUpForm);
+    this.submitted=true;
+    this.user.username=this.signUpForm.value.userData.username;
+    this.user.email=this.signUpForm.value.userData.email;
+    this.user.secretQuestion=this.signUpForm.value.secret;
+    this.user.answer=this.signUpForm.value.QuestionAnswer;
+    this.user.gender=this.signUpForm.value.gender;
+
+    this.signUpForm.reset();
   }
 }
